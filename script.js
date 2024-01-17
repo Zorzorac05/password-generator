ecdf038b09b6bf25226a414f209160781feeb285
Alex Ryder <ajryder31@gmail.com>
2024-01-17 13:44:02 -0700

    all if else for all combo of promts filled added project done

--- script.js
+++ script.js
@@ -30,13 +30,35 @@ function generatePassword() {
 
 // if else to call fillPassword function based on what criteria the user choses
 if(lower && upper && numeric && specialChar){
-  return fillPassword(length, 4);
-} else if(lower && upper && numeric){
-  return fillPassword(length, 3);
-} else if(lower && upper){
-  return fillPassword(length, 2);
-}else if(lower){
   return fillPassword(length, 1);
+} else if(lower && upper && numeric){
+  return fillPassword(length, 2);
+} else if(lower && upper && specialChar){
+  return fillPassword(length, 3);
+}else if(lower && numeric && specialChar){
+  return fillPassword(length, 4);
+}else if(upper && numeric && specialChar){
+  return fillPassword(length, 5);
+}else if(upper && lower){
+  return fillPassword(length, 6);
+}else if(upper && specialChar){
+  return fillPassword(length, 7);
+}else if(upper && numeric){
+  return fillPassword(length, 8);
+}else if(lower && specialChar){
+  return fillPassword(length, 9);
+}else if(lower && numeric){
+  return fillPassword(length, 10);
+}else if(specialChar && numeric){
+  return fillPassword(length, 11);
+}else if(lower){
+  return fillPassword(length, 12);
+}else if(upper){
+  return fillPassword(length, 13);
+}else if(specialChar){
+  return fillPassword(length, 14);
+}else if(numeric){
+  return fillPassword(length, 15);
 }else{
   return "you dont want a password then";
 }
@@ -45,20 +67,111 @@ if(lower && upper && numeric && specialChar){
 
 //function to fill the password given the filled in info
 function fillPassword(length, num){
-  //sets up temp array to fill
+  //sets up temp array to fill and var arrays to get the array to pull from
   const temp = [];
+  var arrays;
  
   //for loop iterates based on the desired password length
   for(var i = 0; i < length; i++){
-    //choses random array to pull from within the critira then uses if to pull from 1 of the 4 arrays
-    var arrays = Math.floor(Math.random() * num);
+    //if elses for each combo of inputs they return
+    if(num == 1){
+      //choses random array to pull from if all are yes
+      arrays = Math.floor(Math.random() * 4);
+    }else if(num ==2){
+      //random number between 1 - 3 then pulls from array within the critera
+      x = Math.floor(Math.random() * 3);
+      if(x == 1){
+        arrays = 1;
+      }else if(x == 2){
+        arrays = 2;
+      }else{
+        arrays = 3;
+      }
+    }else if(num ==3){
+      x = Math.floor(Math.random() * 3);
+      if(x == 1){
+        arrays = 1;
+      }else if(x == 2){
+        arrays = 3;
+      }else{
+        arrays = 4;
+      }
+    }else if(num ==4){
+      x = Math.floor(Math.random() * 3);
+      if(x == 1){
+        arrays = 1;
+      }else if(x == 2){
+        arrays = 2;
+      }else{
+        arrays = 4;
+      }
+    }else if(num ==5){
+      x = Math.floor(Math.random() * 3);
+      if(x == 1){
+        arrays = 2;
+      }else if(x == 2){
+        arrays = 3;
+      }else{
+        arrays = 4;
+      }
+    }else if(num ==6){
+      x = Math.floor(Math.random() * 2);
+      if(x == 1){
+        arrays = 1;
+      }else{
+        arrays = 2;
+      } 
+    }else if(num ==7){
+      x = Math.floor(Math.random() * 2);
+      if(x == 1){
+        arrays = 2;
+      }else{
+        arrays = 4;
+      }
+    }else if(num ==8){
+      x = Math.floor(Math.random() * 2);
+      if(x == 1){
+        arrays = 2;
+      }else{
+        arrays = 3;
+      } 
+    }else if(num ==9){
+      x = Math.floor(Math.random() * 2);
+      if(x == 1){
+        arrays = 1;
+      }else{
+        arrays = 4;
+      }
+    }else if(num ==10){
+      x = Math.floor(Math.random() * 2);
+      if(x == 1){
+        arrays = 1;
+      }else{
+        arrays = 3;
+      }
+    }else if(num ==11){
+      x = Math.floor(Math.random() * 2);
+      if(x == 1){
+        arrays = 3;
+      }else{
+        arrays = 4;
+      }
+    }else if(num ==12){
+      arrays = 1;
+    }else if(num ==13){
+      arrays = 2;
+    }else if(num ==14){
+      arrays = 4;
+    }else if(num ==15){
+      arrays = 3;
+    }
 
-    if(arrays == 0){
+    if(arrays == 1){
       //grabs a random position from the selected array
       temp.push(lowerArray[Math.floor(Math.random() * lowerArray.length)]);
-    }else if(arrays == 1){
-      temp.push(upperArray[Math.floor(Math.random() * upperArray.length)]);
     }else if(arrays == 2){
+      temp.push(upperArray[Math.floor(Math.random() * upperArray.length)]);
+    }else if(arrays == 3){
       temp.push(numericArray[Math.floor(Math.random() * numericArray.length)]);
     }else {
       temp.push(specialCharArray[Math.floor(Math.random() * specialCharArray.length)]);
